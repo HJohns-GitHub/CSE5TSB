a116af5165843de81eb3847e58a3021b090d1afc
HJohns-GitHub <henry.johns@outlook.com.au>
2024-08-12 18:43:25 +1000

    updated visualisation

--- KPGNN.py
+++ KPGNN.py
@@ -1,6 +1,6 @@
 #import os
 import tarfile
-import pandas as pd
+import pandas
 #import protein DB data
 from io import StringIO
 import pytorch
@@ -141,3 +141,213 @@ def plot_output(data, output):
 #Example
 LRP(model, graph_data, torch.tensor([0]))
 plot_output(graph_data, output)
+
+if(length(outputs) == 1){
+  write.tsv(settings, file = paste0(output.folder, "/", "NodeWeights_MetaData.tsv"))
+  write.csv(toMT(nodeWeights, row = "Node", col="replicate", val = "value"), file = paste0(output.folder, "/", "NodeWeights.csv"), quote=F)
+} else {
+  nodeWeights[,id := paste0(replicate, "_", variable)]
+  write.csv(toMT(nodeWeights, row = "Node", col="id", val = "value"), file = paste0(output.folder, "/", "NodeWeights.csv"), quote=F)
+  
+  settings2 <- data.table()
+  for(i in 1:nrow(settings)){
+    xx <- unique(nodeWeights[replicate == settings[i]$replicate][,c("replicate", "variable", "id")])
+    settings2 <- rbind(settings2, data.table(settings[i][,-"replicate"], replicate = xx$id, outputNode=xx$variable))
+  }
+  write.tsv(settings2, file = paste0(output.folder, "/", "NodeWeights_MetaData.tsv"))
+}
+
+def __init__(self, model, samples, labels):
+        """
+        Initialization of internals for relevance computations.
+        :param model: gcnn model to LRP procedure is applied on
+        :param samples: samples to calculate relevance on, num of samples <= models batch size
+        :param labels: used as values for the gcnn output nodes to propagate relevances, num of labels == models batch size
+        """
+        self.epsilon = 1e-10  # for numerical stability
+
+        start = time.time()
+        print("\n\tCalculating Polynomials of Laplace Matrices...", end=" ")
+        self.polynomials = [self.calc_Laplace_Polynom(lap, K=model.K[i]) for i, lap in enumerate(model.L)]
+        end = time.time()
+        print("Time: ", end - start, "\n")
+
+        weights = model.get_weights()
+        self.activations = model.activations
+
+        self.model = model
+        self.model.graph._unsafe_unfinalize()  # the computational graph of the model will be modified
+        self.labels = labels
+        self.samples = samples
+        self.X = self.activations[0]  # getting the first
+
+        # self.y = self.activations.pop(0)
+        # I am getting the activation of the first, but not useful here, self.y will be assigned a placeholder
+
+        self.ph_dropout = model.ph_dropout
+        self.batch_size = self.X.shape[0]
+
+        with self.model.graph.as_default():
+            self.y = tf.placeholder(tf.float32, (self.batch_size, labels.shape[1]), 'labels_hot_encoded')
+
+        self.act_weights = {}  # example in this dictionary "conv1": [weights, bias]
+
+        for act in self.activations[1:]:
+            w_and_b = []  # 2 element list of weight and bias of one layer.
+            name = act.name.split('/')
+            # print(name)
+            for wt in weights:
+                # print(wt.name)
+                if name[0] == wt.name.split('/')[0]:
+                    w_and_b.append(wt)
+            if w_and_b and (name[0] not in self.act_weights):
+                self.act_weights[name[0]] = w_and_b
+
+        # !!!
+        # first convolutional layer filters
+        self.filters_gc1 = []
+        self.filtered_signal_gc1 = []
+
+    def get_relevances(self):
+        """
+        Computes relevances based on input samples.
+        :param rule: the propagation rule of the first layer
+        :return: the list of relevances, corresponding to different layers of the gcnn.
+        The last element of this list contains input relevances and has shape (batch_size, num_of_input_features)
+        """
+
+        # Backpropagate softmax value
+        # relevances = [tf.nn.softmax(self.activations[0])*tf.cast(self.y, tf.float32)]
+
+        # Backpropagate a value from given labels y
+        relevances = [tf.cast(self.y, tf.float32)]
+
+        loc_poly = [pol for pol in self.polynomials]
+        loc_pooling = [p for p in self.model.p]
+        print("\n    Relevance calculation:")
+
+        for i in range(len(self.activations)-2, -1, -1):  # reverse order of the activations and excluding the very
+            # first activation 'ExpandDims:0'
+            name = self.activations[i+1].name.split('/')
+
+            # print("\n\t name of the activation", name)
+
+            if 'logits' in name[0] or 'fc' in name[0]:
+                print("\tFully connected:", name[0])
+                relevances.append(self.prop_fc(name[0], self.activations[i], relevances[-1]))
+            elif 'flatten' in name[0]:
+                print("\tFlatten layer:", name[0])
+                relevances.append(self.prop_flatten(self.activations[i], relevances[-1]))
+                # print("\n")
+            elif 'pooling' in name[1]:
+                # TODO: incorporate pooling type and value into name
+                print("\tPooling:", name[0] + " " + name[1])
+                print("\t\tname of pooling:", self.model.pool.__name__)
+                if self.model.pool.__name__ == 'apool1':
+                    p = loc_pooling.pop()
+                    relevances.append(self.prop_avg_pool(self.activations[i], relevances[-1], ksize=[1, p, 1, 1],
+                                                         strides=[1, p, 1, 1]))
+                elif self.model.pool.__name__ == 'mpool1':
+                    p = loc_pooling.pop()
+                    relevances.append(self.prop_max_pool(self.activations[i], relevances[-1], ksize=[1, p, 1, 1],
+                                                         strides=[1, p, 1, 1]))
+                else:
+                    raise Exception('Error parsing the pooling type')
+
+            elif 'conv' in name[0]:
+                if len(loc_poly) > 1:
+                    print("\tConvolution: ", name[0], "\n")
+                    relevances.append(self.prop_gconv(name[0], self.activations[i], relevances[-1],
+                                                      polynomials=loc_poly.pop()))
+                else:
+                    print("\tConvolution, the first layer:", name[0], "\n")
+                    relevances.append(self.prop_gconv_first_conv_layer(name[0], self.activations[i], relevances[-1],
+                                                                       polynomials=loc_poly.pop()))
+            else:
+                raise Exception('Error parsing layer')
+
+        return relevances
+
+    def prop_fc(self, name, activation, relevance):
+        """Propagates relevances through fully connected layers."""
+        w = self.act_weights[name][0]
+        # b = self.act_weights[name][1]  # bias
+        w_pos = tf.maximum(0.0, w)
+        z = tf.matmul(activation, w_pos) + self.epsilon
+        s = relevance / z
+        c = tf.matmul(s, tf.transpose(w_pos))
+        return c * activation
+
+    def prop_flatten(self, activation, relevance):
+        """Propagates relevances from the fully connected part to convolutional part."""
+        shape = activation.get_shape().as_list()
+        return tf.reshape(relevance, shape)
+
+    def prop_max_pool(self, activation, relevance, ksize=[1, 2, 1, 1], strides=[1, 2, 1, 1]):
+        """Propagates relevances through max pooling."""
+        act = tf.expand_dims(activation, 3)  # N x M x F x 1
+        z = tf.nn.max_pool(act, ksize, strides, padding='SAME') + self.epsilon
+        with self.model.graph.as_default():
+            rel = tf.expand_dims(relevance, 3)
+        s = rel / z
+        c = gen_nn_ops.max_pool_grad_v2(act, z, s, ksize, strides, padding='SAME')
+        tmp = c * act
+        return tf.squeeze(tmp, [3])
+
+    def prop_avg_pool(self, activation, relevance, ksize=[1, 2, 1, 1], strides=[1, 2, 1, 1]):
+        """Propagates relevances through avg pooling."""
+        act = tf.expand_dims(activation, 3)  # N x M x F x 1
+        z = tf.nn.avg_pool(act, ksize, strides, padding='SAME') + self.epsilon
+        with self.model.graph.as_default():
+            rel = tf.expand_dims(relevance, 3)
+        s = rel / z
+        c = gen_nn_ops.avg_pool_grad(tf.shape(act), s, ksize, strides, padding='SAME')
+        tmp = c * act
+        return tf.squeeze(tmp, [3])
+
+    def prop_gconv(self, name, activation, relevance, polynomials):
+        """
+        Perform relevance propagation through Graph Convolutional Layers.
+        All essential operations are in SCIPY.
+        """
+        start = time.time()
+        w = self.act_weights[name][0]  # weight
+        b = self.act_weights[name][1]  # bias
+        # print("\nInside gconv")
+        # print("weights of current gconv, w:", w)
+        # activation
+        N, M, Fin = activation.get_shape()
+        N, M, Fin = int(N), int(M), int(Fin)
+        Fout = int(w.get_shape().as_list()[-1])
+
+        K = int(w.get_shape().as_list()[0] / Fin)
+        W = self.model._get_session().run(w)
+       
+        activation = self.run_tf_tensor(activation, samples=self.samples)
+
+        
+        if tf.is_tensor(relevance):
+            relevance = self.run_tf_tensor(relevance, self.samples)
+
+        W = np.reshape(W, (int(W.shape[0] / K), K, Fout))
+        W = np.transpose(W, (1, 0, 2))  # K x Fin x Fout
+
+        rel = np.zeros(shape=[N, M * Fin], dtype=np.float32)
+        for i in range(0, Fout):
+            w_pos = polynomials.dot(W[:, :, i])
+            w_pos = np.maximum(0.0, w_pos)
+            w_pos = np.reshape(w_pos, [M, M, Fin])
+            w_pos = np.transpose(w_pos, axes=[0, 2, 1])  # M x Fin x M
+            w_pos = np.reshape(w_pos, [M * Fin, M])
+            activation = np.reshape(activation, [N, Fin * M])  # N x Fin*M
+            z = np.matmul(activation, w_pos) + self.epsilon  # N x M
+            s = relevance[:, :, i] / z  # N x M
+            c = np.matmul(s, np.transpose(w_pos))  # N x M by transpose(M * Fin, M) = N x M * Fin
+            rel += c * activation
+        end = time.time()
+        # #
+        rel = np.reshape(rel, [N, M, Fin])
+        print("\n\t" + name + ",", "relevance propagation time is: ", end - start)
+
+        return rel
+
