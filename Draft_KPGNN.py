b6807822bbff823d40e2478b5b2f6b1f2c90a86f
HJohns-GitHub <henry.johns@outlook.com.au>
2024-08-27 18:21:02 +1000

    Add files via upload

---
+++ Draft_KPGNN.py
@@ -0,0 +1,18 @@
+import os
+import tarfile
+import pandas
+#import protein DB data
+from io import StringIO
+import tensorflow 
+#import torch
+#import torch.nn.functional as F
+#from torch_geometric.data import data
+#from torch_geometric.nn import GCNConv
+#import torch.optim as optim
+import sys
+import time
+import scipy
+import Pylance
+#for plotting GNN output in 2D
+from matplotlib import pyplot as plt
+from random import randint
